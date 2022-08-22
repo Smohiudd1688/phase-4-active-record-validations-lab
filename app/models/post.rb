4f7d697961e1201414aacad2bb58418369ccd305
Sana Mohiuddin <smohiuddin1688@gmail.com>
2022-08-21 19:32:25 -0700

    commit tree e80f1504dfad79422fde7c3c6863a198ae912fc5

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+    validate :must_be_clickbaity
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction), message: "is not included in the list"}
+
+    def must_be_clickbaity
+        unless title =~ /Won't Believe/ || /Secret/ || /Guess/ ||  /(Top) [0-9]/
+            errors.add(:title, "not clickbaity enough")
+        end
+    end
+    
 end
